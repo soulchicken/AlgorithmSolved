7a41e147256214dbefc53bd2408a421eab917c55
소울치킨 <soulfever01@naver.com>
2022-01-21 22:27:16 +0900

    [코드업 기초 100제] 기초 - 선택실행구조

---
+++ CodeUp 기초 100제/6066_정수 3개 입력받아 짝, 홀 출력하기.py
@@ -0,0 +1,17 @@
+a, b, c = map(int,input().split())
+even = 'even'
+odd = 'odd'
+if a % 2 == 0:
+    print(even)
+else:
+    print(odd)
+    
+if b % 2 == 0:
+    print(even)
+else:
+    print(odd)
+    
+if c % 2 == 0:
+    print(even)
+else:
+    print(odd)
