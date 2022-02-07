4c5f10f015b7a7e701220d1e54bf48b34ae95091
소울치킨 <soulfever01@naver.com>
2022-02-07 15:54:15 +0900

    [프로그래머스] 핸드폰 번호 가리기 (Level1) JAVA

---
+++ Level1_JAVA_Programmers/핸드폰 번호 가리기/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution("01033334444"));
+        System.out.println(solution.solution("027778888"));
+    }
+}
