78e873f6b20a84b9713250e91182ca8e213e89ca
소울치킨 <soulfever01@naver.com>
2022-02-08 13:19:40 +0900

    [프로그래머스] 하샤드 수 (Level1) JAVA

---
+++ Level1_JAVA_Programmers/하샤드 수/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    static Solution solution = new Solution();
+
+    public static void main(String[] args) {
+        System.out.println(solution.solution(10));
+        System.out.println(solution.solution(12));
+        System.out.println(solution.solution(11));
+        System.out.println(solution.solution(13));
+    }
+}
