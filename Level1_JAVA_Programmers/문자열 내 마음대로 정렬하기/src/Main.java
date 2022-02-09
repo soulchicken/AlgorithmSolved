a0ab1f8ce376ad9b119f6b62ae63b80ebdab7fce
소울치킨 <soulfever01@naver.com>
2022-02-09 16:31:41 +0900

    [프로그래머스] 문자열 내 마음대로 정렬하기 (Level1) JAVA

---
+++ Level1_JAVA_Programmers/문자열 내 마음대로 정렬하기/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.solution(new String[]{"sun", "bed", "car"}, 1)));
+        System.out.println(Arrays.toString(s.solution(new String[]{"abce", "abcd", "cdx"}, 2)));
+    }
+}
