5712831c62ed1f603be495d22a6029ba69a4dc09
소울치킨 <soulfever01@naver.com>
2022-02-10 15:03:07 +0900

    [프로그래머스] 같은 숫자는 싫어 (Level1) JAVA

---
+++ Level1_JAVA_Programmers/같은 숫자는 싫어/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.solution(new int[]{1, 1, 3, 3, 0, 1, 1})));
+        System.out.println(Arrays.toString(s.solution(new int[]{4, 4, 4, 3, 3})));
+    }
+}
