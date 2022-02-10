57c719f6aa7e24f9aa0082f022ca1d67d67bc94a
소울치킨 <soulfever01@naver.com>
2022-02-10 14:54:29 +0900

    [프로그래머스] 나누어 떨어지는 숫자 배열 (Level1) JAVA

---
+++ Level1_JAVA_Programmers/나누어 떨어지는 숫자 배열/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.solution(new int[]{5, 9, 7, 10}, 5)));
+        System.out.println(Arrays.toString(s.solution(new int[]{2, 36, 1, 3}, 1)));
+        System.out.println(Arrays.toString(s.solution(new int[]{3, 2, 6}, 10)));
+    }
+}
