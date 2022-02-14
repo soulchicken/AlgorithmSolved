411456263519de578e79b291cd218822e8313b54
소울치킨 <soulfever01@naver.com>
2022-02-14 13:55:42 +0900

    [프로그래머스] 음양 더하기 (Level1) JAVA

---
+++ Level1_JAVA_Programmers/음양 더하기/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(new int[] {4,7,12},new boolean[] {true,false,true}));
+        System.out.println(s.solution(new int[] {1,2,3}, new boolean[] {false,false,true}));
+    }
+}
