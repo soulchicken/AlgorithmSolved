82406a70bbda52a673ebf5d3ee42ae221eed4925
소울치킨 <soulfever01@naver.com>
2022-02-14 13:58:19 +0900

    [프로그래머스] 없는 숫자 더하기 (Level1) JAVA

---
+++ Level1_JAVA_Programmers/없는 숫자 더하기/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(new int[] {1,2,3,4,6,7,8,0}));
+        System.out.println(s.solution(new int[] {5,8,4,0,6,7,9}));
+    }
+}
