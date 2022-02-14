abb2321ff58f5edc49bd0ae1ee37e2d466e016ee
소울치킨 <soulfever01@naver.com>
2022-02-14 14:02:14 +0900

    [프로그래머스] 내적 (Level1) JAVA

---
+++ Level1_JAVA_Programmers/내적/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(new int[] {1,2,3,4},new int[] {-3,-1,0,2}));
+        System.out.println(s.solution(new int[] {-1,0,1},new int[] {1,0,-1}));
+    }
+}
