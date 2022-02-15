382270fdae5d80a4e998e65e30efb967951148fe
소울치킨 <soulfever01@naver.com>
2022-02-15 14:56:51 +0900

    [프로그래머스] 숫자 문자열과 영단어 (Level1) JAVA

---
+++ Level1_JAVA_Programmers/숫자 문자열과 영단어/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution("one4seveneight"));
+        System.out.println(s.solution("23four5six7"));
+        System.out.println(s.solution("2three45sixseven"));
+        System.out.println(s.solution("123"));
+    }
+}
